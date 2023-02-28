622080adf9b60d3bcd04da3c10be0efd18362642
Shiven Desai <shiven.desai14@gmail.com>
2023-02-27 19:19:37 -0800

    Made part 3

---
+++ excersise3-part3.py
@@ -0,0 +1,5 @@
+#Written by Shiven Desai
+#This program prints odd and even numbers until 10
+print('I will display the numbers 1 through 10.')
+for num in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print(num)
